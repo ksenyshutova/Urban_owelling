4050a9ec925dfc08e29902edefce789c8cbc50df
ksenyshutova <shutovasn@mail.ru>
2024-08-08 08:29:50 +0300

    correct

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,12 @@
 
 let item = document.querySelectorAll('.services__item_basket');
 let count = document.querySelector('.form__count');
+let btn = document.querySelectorAll('.service-card__btn');
 
 item.forEach(el =>{
         count.textContent = Number(count.textContent) + 1;
 });
+
+btn.oncklick = function(event) {
+        event.preventDefault;
+}
